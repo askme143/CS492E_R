c4a3b86158257ef206b6560eb5b4679b178d62ec
Yeongil Yoon <askme143@kaist.ac.kr>
2020-06-17 03:48:23 +0900

    Add plot

---
+++ cs496e/idol_week_analysis.R
@@ -0,0 +1,50 @@
+library(dplyr)
+
+week_to_date = function(nth_week) {
+  start = as.Date("2019-06-02", "%Y-%m-%d")
+  start + nth_week*7
+}
+
+celeb_list = read.csv("직업군.csv", header = T)
+
+for (celeb in celeb_list$idol){
+  celeb_csv = read.csv(paste0("data/", celeb, "_data.csv"))
+  colnames(celeb_csv) = c("celeb","title","article_type","nth_week","year","month","date","time","total","like","warm","sad","angry","want","cheer","congrats","expect","surprise","fan","url")
+  celeb_csv[9:19] = sapply(celeb_csv[9:19], as.numeric)
+  celeb_csv = celeb_csv %>%
+    mutate(week_start = week_to_date(nth_week))
+  
+  # Total emos per week
+  total_week = celeb_csv %>%
+    select(nth_week, total) %>%
+    filter(total>30) %>%
+    group_by(nth_week) %>%
+    summarize(mean_total = mean(total))
+  png(filename=paste0("idol_plot/","주간관심도_", celeb, ".png"))
+  plot(total_week, main = paste(celeb, "주간 관심도"), ylim=c(30, 1000))
+  dev.off()
+  
+  emos = celeb_csv %>%
+    filter(total>30) %>%
+    .[10:19]
+  pie(x = apply(emos, 2, mean))
+  
+  # Positive, negative emotions per week
+  pos_neg_week = celeb_csv %>%
+    filter(total>30) %>%
+    mutate(positive = total-sad-surprise-angry,
+           negative = total - positive) %>%
+    select(nth_week, positive, negative, total) %>%
+    mutate(ratio = positive/total) %>%
+    select(nth_week, ratio) %>%
+    group_by(nth_week) %>%
+    summarize(ratio = mean(ratio))
+  
+  # Positive ratio per week
+  ratio_week = pos_neg_week %>%
+    select (nth_week, ratio)
+  png(filename=paste0("idol_plot/","주간긍정비율_", celeb, ".png"))
+  plot(ratio_week, main=paste(celeb, "주간 긍정 비율"), ylim = c(0, 1))
+  dev.off()
+}
+
